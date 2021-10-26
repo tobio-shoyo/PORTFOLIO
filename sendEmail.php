0657072cd001bfdc5f52f8eafc043398591bbe33
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-10-26 19:22:32 +0530

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,41 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $body = $_POST['body'];
+
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
+
+        $mail = new PHPMailer();
+
+        //SMTP Settings
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "ankushsahoo004@gmail.com"; //enter your email address
+        $mail->Password = 'roman@123'; //enter your email password
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($email, $name);
+        $mail->addAddress("ankushsahoo004@gmail.com"); //enter your email address
+        $mail->Subject = ("$email");
+        $mail->Body = $body;
+
+        if ($mail->send()) {
+            $status = "success";
+            $response = "Email is sent!";
+        } else {
+            $status = "failed";
+            $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+        }
+
+        exit(json_encode(array("status" => $status, "response" => $response)));
+    }
+?>
